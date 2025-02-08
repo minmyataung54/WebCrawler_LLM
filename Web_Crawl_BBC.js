869efb6ac7efdcdede9ef2178fba4b4076c5b35a
AungKhantThaw47 <aungkthaw@gmail.com>
2025-02-08 22:10:25 +0700

    BBC Meta

--- Web_Crawl_BBC.js
+++ Web_Crawl_BBC.js
@@ -4,21 +4,18 @@ const fs = require("fs");
 
 // BBC Burmese URL
 const url = "https://www.bbc.com/burmese/topics/c404v08p1wxt";
-const totalPage = 200;
+const totalPage = 500;
+const filePath = "BBC.json";
 
 // Define date filter (e.g., only scrape articles from 2024)
-const filterDate = new Date("2024-10-20"); // Change this to your desired date
+const filterDate = new Date("2024-01-01"); // Change this to your desired date
+const postdata = [];
 
 async function scrapePage(baseUrl , url, page) {
-    if (page > totalPage) {
-        return;
-    }
     try {
         const { data } = await axios.get(url);
         const $ = cheerio.load(data);
 
-        // Extract posts
-        const postdata = [];
         $("div.promo-text").each((i, el) => {
             const title = $(el).find("a").text().trim();
             const dateStr = $(el).find("time").attr("datetime"); // Extract datetime attribute
@@ -36,15 +33,16 @@ async function scrapePage(baseUrl , url, page) {
 
         console.log(postdata);
 
-        // Save filtered data
-        fs.appendFileSync("BBC.txt", `${JSON.stringify(postdata)}\n`, "utf8");
-
         // Get the next page URL
         const nextUrl = $('nav > span > a[aria-labelledby="pagination-next-page"]').attr("href");
         
-        if (nextUrl && postdata.length > 0) {
+        if (nextUrl && postdata.length > 0 && page < totalPage) {
             console.log("Next page:", baseUrl + nextUrl);
             scrapePage(baseUrl ,baseUrl + nextUrl, page + 1);
+        }else{
+            console.log("Scraping completed.");
+            // Save filtered data
+            fs.appendFileSync(filePath, `${JSON.stringify(postdata)}\n`, "utf8");
         }
 
     } catch (error) {
@@ -52,5 +50,9 @@ async function scrapePage(baseUrl , url, page) {
     }
 }
 
+if (fs.existsSync(filePath)) {
+    // Delete the file if it exists
+    fs.unlinkSync(filePath);
+  }
 // Start scraping
 scrapePage(url ,url, 1);
